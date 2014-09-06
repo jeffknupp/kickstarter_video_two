861cc50e738fda49824c79e67ac88ed48b815803
Jeffery Knupp <jknupp@jefferys-mbp.home>
2014-09-06 10:57:18 -0400

    Refactor use of class attributes

--- proxy.py
+++ proxy.py
@@ -7,9 +7,9 @@ import thread
 import select
 
 __version__ = '0.1.0 Draft 1'
-BUFLEN = 8192
-VERSION = 'Python Proxy/'+__version__
-HTTPVER = 'HTTP/1.1'
+BUFFER_LENGTH = 8192
+VERSION = 'Python Proxy/{}'.format(__version__)
+HTTP_VERSION = 'HTTP/1.1'
 
 
 class ConnectionHandler(object):
@@ -19,12 +19,12 @@ class ConnectionHandler(object):
         self.client_buffer = ''
         self.timeout = timeout
         self.target = None
-        self.method, self.path, self.protocol = self.get_base_header()
-        if self.method == 'CONNECT':
-            self.method_connect()
-        elif self.method in ('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT',
-                             'DELETE', 'TRACE'):
-            self.method_others()
+        method, path, protocol = self.get_base_header()
+        if method == 'CONNECT':
+            self.method_connect(path)
+        elif method in ('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT',
+                        'DELETE', 'TRACE'):
+            self.method_others(method, path, protocol)
         self.client.close()
         self.target.close()
 
@@ -32,7 +32,7 @@ class ConnectionHandler(object):
         """Return a tuple of (method, path, protocol) from the recieved
         message."""
         while 1:
-            self.client_buffer += self.client.recv(BUFLEN)
+            self.client_buffer += self.client.recv(BUFFER_LENGTH)
             end = self.client_buffer.find('\n')
             if end != -1:
                 break
@@ -41,27 +41,27 @@ class ConnectionHandler(object):
         self.client_buffer = self.client_buffer[end+1:]
         return data
 
-    def method_connect(self):
+    def method_connect(self, path):
         """Handle HTTP CONNECT messages."""
-        self._connect_target(self.path)
-        self.client.send('{httpver} 200 Connection established\n'
+        self._connect_target(path)
+        self.client.send('{http_version} 200 Connection established\n'
                          'Proxy-agent: {version}\n\n'.format(
-                             httpver=HTTPVER,
+                             http_version=HTTP_VERSION,
                              version=VERSION))
         self.client_buffer = ''
         self._read_write()
 
-    def method_others(self):
+    def method_others(self, method, path, protocol):
         """Handle all non-HTTP CONNECT messages."""
-        self.path = self.path[7:]
-        i = self.path.find('/')
-        host = self.path[:i]
-        path = self.path[i:]
+        path = path[7:]
+        i = path.find('/')
+        host = path[:i]
+        path = path[i:]
         self._connect_target(host)
         self.target.send('{method} {path} {protocol}\n{client_buffer}'.format(
-            method=self.method,
+            method=method,
             path=path,
-            protocol=self.protocol,
+            protocol=protocol,
             client_buffer=self.client_buffer))
         self.client_buffer = ''
         self._read_write()
@@ -91,7 +91,7 @@ class ConnectionHandler(object):
                 break
             if recv:
                 for in_ in recv:
-                    data = in_.recv(BUFLEN)
+                    data = in_.recv(BUFFER_LENGTH)
                     if in_ is self.client:
                         out = self.target
                     else:
